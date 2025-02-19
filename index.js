32daefc905229f41d14ff2e96b8af837edd54086
mehdiakbarifar <akbarifar@gmail.com>
2025-02-19 09:08:56 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const telegramUrl = 'https://api.telegram.org/bot<TOKEN>/';
+    const url = new URL(request.url);
+    const path = url.pathname.split('/').pop();
+    const newUrl = `${telegramUrl}${path}`;
+    const init = {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+    };
+    const response = await fetch(newUrl, init);
+    return response;
+}
