67ca03bafacd2bf843b4016d8e742bb7954273af
mehdiakbarifar <akbarifar@gmail.com>
2025-02-19 15:45:44 +0330

    Update telegram_worker.js

--- telegram_worker.js
+++ telegram_worker.js
@@ -3,8 +3,7 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
-    // Access the environment variable
-    const TELEGRAM_BOT_TOKEN = globalThis.TELEGRAM_BOT_TOKEN; 
+    const TELEGRAM_BOT_TOKEN = globalThis.TELEGRAM_BOT_TOKEN; // Access the environment variable
     const telegramUrl = 'https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN + '/';
     const url = new URL(request.url);
     const path = url.pathname.split('/').pop();
